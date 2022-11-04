d6e863be774caef25b8d9d4f833b90771b61316e
DinoVincent <106249505+DinoVincent@users.noreply.github.com>
2022-11-04 21:23:42 +0100

    LeanTween on canvas

--- Assets/Scripts/Tab.cs
+++ Assets/Scripts/Tab.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class Tab : MonoBehaviour
 {
     [SerializeField]
-    GameObject missionMenu;
+    GameObject missionMenu, backdrop;
+    bool _open;
+    [SerializeField]
+    CanvasGroup Mission;
+    
+
+
+    void Start(){
+        if(missionMenu.activeSelf) _open = true; else _open = false;
+    }
+
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if (missionMenu.activeSelf)
+            if (_open)
             {
-                missionMenu.SetActive(false);
+                Mission.alpha=1f;
                 Cursor.lockState = CursorLockMode.Locked;
-               
+                LeanTween.alphaCanvas(Mission, 0f, .2f);
+                LeanTween.scale(backdrop, new Vector3(.5f,.5f,.5f), .15f);
+                _open=false;
 
             }
             else
-            {
+            {   missionMenu.SetActive(false);
+                Mission.alpha=0f;
                 missionMenu.SetActive(true);
                 Cursor.lockState = CursorLockMode.None;
+                LeanTween.alphaCanvas(Mission, 1f, .2f);
+                LeanTween.scale(backdrop, new Vector3(1,1,1), .15f);
+                _open=true;
             }
         }
     }
