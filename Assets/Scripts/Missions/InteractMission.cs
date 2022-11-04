e85c8c6d9e96e2e4cb7da3804990ae40081c7bc5
DinoVincent <106249505+DinoVincent@users.noreply.github.com>
2022-11-04 01:50:19 +0100

    Missions

--- Assets/Scripts/Missions/InteractMission.cs
+++ Assets/Scripts/Missions/InteractMission.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class InteractMission : MonoBehaviour
+[System.Serializable]
+public class InteractMission
 {
+    public GameObject target;
+    public GameObject canvasTooltip;
+    public bool RemoveTarget;
+    public float Range;
+    public LayerMask Hitmask;
+    private Camera _cam;
+    [System.NonSerialized]
     public Submissiondisplay subd;
  public void updateMission(SubMission sub){
+    if (_cam == null) _cam = Camera.main;
     if(sub.state == missionclass.missionState.Ongoing){
+        Ray(sub);
         updateUI(sub);
+        
     }
     if(sub.state != missionclass.missionState.Completed || sub.state != missionclass.missionState.Hidden){
       updateUI(sub);
     }
   }
+
+    void Ray(SubMission sub){
+      RaycastHit hit;
+      if(Physics.Raycast(_cam.transform.position, _cam.transform.forward,out hit, Range, Hitmask)){
+          if(hit.collider.gameObject == target){
+            canvasTooltip.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.E)){
+              if(RemoveTarget)  target.SetActive(false);
+              sub.state = missionclass.missionState.Completed;
+              canvasTooltip.SetActive(false);
+              updateUI(sub);
+            }
+          }
+      }else{
+            canvasTooltip.SetActive(false);
+          }
+    }
+
+
 
   public void updateUI(SubMission sub, HeadMission hd=null){
     if (subd == null) subd=sub.visual.GetComponent<Submissiondisplay>();
@@ -26,10 +55,7 @@ public class InteractMission : MonoBehaviour
     else if(sub.state == missionclass.missionState.Completed)
     subd.updateProgress("Completed", 1f, Submissiondisplay.colorOption.completed);
     else if(sub.state == missionclass.missionState.Ongoing){
-      
       subd.updateProgress("TO-DO", 0f, Submissiondisplay.colorOption.progressc);
-
-      
     
     }
   }
